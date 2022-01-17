3da51f006ee00b877b6ac7668b28dc8c580f89b0
aqandrew <dawneraq@gmail.com>
2022-01-17 15:55:37 -0800

    fix build errors for post page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,22 +1,21 @@
 import { fetchEntries, fetchPost } from '@utils/contentfulPosts'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
-export default function Post({ post }) {
-  const category = post.category.fields
-
+export default function Post({ post, category }) {
   return (
     <>
-      <h1>{post.title}</h1>
-      <p>Category: {category.title}</p>
-      {documentToReactComponents(post.body)}
+      {post ? <h1>{post.title}</h1> : null}
+      {category ? <p>Category: {category.title}</p> : null}
+      {post ? documentToReactComponents(post.body) : null}
     </>
   )
 }
 
 export async function getStaticProps({ params }) {
-  const post = await fetchPost(params.slug)
+  const post = (await fetchPost(params.slug)).fields
+  const category = post.category.fields
 
-  return { props: { post: post.fields } }
+  return { props: { post, category } }
 }
 
 export async function getStaticPaths() {
